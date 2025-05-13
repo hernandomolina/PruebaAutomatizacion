714837d6fa82db80ed6b7345fbd8b2b9e2761d17
Didier Arias <didier.arias@dreamcodesoft.com>
2025-05-13 11:24:25 -0500

    Test1

--- src/test/java/com/pruebatecnicaqa/stepdefinitions/OpenPageStepDefinitions.java
+++ src/test/java/com/pruebatecnicaqa/stepdefinitions/OpenPageStepDefinitions.java
@@ -1,17 +1,20 @@
 package com.pruebatecnicaqa.stepdefinitions;
 
 import com.pruebatecnicaqa.pages.WebTablesPage;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
+import net.serenitybdd.annotations.Steps;
+import net.serenitybdd.core.pages.PageObject;
 
 public class OpenPageStepDefinitions {
 
+    @Steps
     WebTablesPage webTablesPage;
 
+    @Before
     @Given("que el usuario abre la página de web tables")
-    public void abrirPagina() {
-        webTablesPage = new WebTablesPage();
+    public void verificarPaginaAbierta() {
+        // Verificar que algún elemento clave de la página está visible
         webTablesPage.openWebTablesPage();
     }
 }
